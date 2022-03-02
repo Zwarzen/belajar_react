a4ecd43af8b4ae21746d4514550d2f52bd9a57a2
Risang <56747783+Zwarzen@users.noreply.github.com>
2022-03-02 08:05:07 +0700

    b - prop

--- pertemuan-ketiga/web-reactku/src/index.js
+++ pertemuan-ketiga/web-reactku/src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from 'App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
 //import HelloComponent from './component/HelloComponent';
 //import * as serviceWorker from './serviceWorker'
 
-function HelloComponent(){
-    return HelloComponent
-ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+const HelloComponent()=>{
+    return HelloComponent;
 }
 
+class.StateFullComponent extends React.Component{
+    render(){
+        return<p>StateFullComponent</p>
+    }
+}
+
+ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
