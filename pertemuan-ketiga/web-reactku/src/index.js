b8a9a8a6e3c9967b8abf04b76841e18471f2976c
Risang <56747783+Zwarzen@users.noreply.github.com>
2022-03-02 07:46:42 +0700

    prak a - statefull stateless

--- pertemuan-ketiga/web-reactku/src/index.js
+++ pertemuan-ketiga/web-reactku/src/index.js
@@ -1,37 +1,18 @@
-import React /*{ Component }*/ from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-//import Header from './Header';
-//import Footer from './Footer';
 import './index.css';
-//import App from './App';
+//import App from 'App';
 import reportWebVitals from './reportWebVitals';
-import { render } from '@testing-library/react';
+//import HelloComponent from './component/HelloComponent';
+//import * as serviceWorker from './serviceWorker'
 
-  /*ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );*/
-  
-  /*
-  function HelloComponent(){
+function HelloComponent(){
     return HelloComponent
-    //ReactDOM.render(<HelloComponent/>, document.getElementById('root'));
-  }
-
-  class.StateFullComponent extends React.components{
-    render(){
-      return <p>StateFullComponent</p>
-    }
-  }
-
-  ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
-  */
-
-  
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//serviceWorker.unregistered();
