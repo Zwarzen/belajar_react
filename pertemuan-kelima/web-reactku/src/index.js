50db4dfa9b311ee4f63319f25b67ac460cea7f1f
Risang <56747783+Zwarzen@users.noreply.github.com>
2022-03-16 07:50:24 +0700

    pertemuan ke 5

--- pertemuan-kelima/web-reactku/src/index.js
+++ pertemuan-kelima/web-reactku/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-import BlogPost from "./container/Blogpost/BlogPost"
-
-ReactDOM.render(<BlogPost />, document.getElementById('content'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
