ff5dca0dbdb772a520f37acf2e33c195c23f6493
Risang <56747783+Zwarzen@users.noreply.github.com>
2022-04-13 08:55:35 +0700

    revisi profil dan foto list produk

--- uts-market2/React-Redux-shopping-app/src/components/profile/Profiles.jsx
+++ uts-market2/React-Redux-shopping-app/src/components/profile/Profiles.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Profiles.css';
-
-
 const HeroBanner = () => {
 
     return (
+        
         <div className="hero-banner">
+            
             {/* <div className="col-3 left-banner"></div> */}
+            
             <div className="text ">
+            <img src='../../media/profile.png' />
                 <h1>Risang Daniswara</h1>
                 <p>1941720166</p>
                 <p>TI-3D</p>
