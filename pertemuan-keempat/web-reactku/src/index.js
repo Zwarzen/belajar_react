d2a51a9445580fb8eadfc3e9f801819699047613
Risang <56747783+Zwarzen@users.noreply.github.com>
2022-04-23 00:14:38 +0700

    error

--- pertemuan-keempat/web-reactku/src/index.js
+++ pertemuan-keempat/web-reactku/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import BlogPost from "./container/Blogpost/BlogPost"
 
 import BlogPostMahasiswa from "./container/Blogpost/BlogPostMahasiswa"
 
-ReactDOM.render(<BlogPostMahasiswa />, document.getElementById('content'))
+ReactDOM.render(<BlogPost />, document.getElementById('content'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
